3668a51e2ecee4b3315cad5e5d59639a0ec63f0d
Gauraang P. Kelkar <gauraangkelkar@gmail.com>
2020-04-09 17:17:02 +0530

    Updated MakeMyAvatar, changed backgroud, added text, added attribute

---
+++ src/components/Loader.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <>
+      <div className="lds-ripple">
+        <div></div>
+        <div></div>
+      </div>
+      <div>Making the awwwwwsome you...!! ;)</div>
+    </>
+  );
+};
+
+export default Loader;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "tachyons";
 import Form from "../components/Form";
 import ImageDisplay from "../components/ImageDisplay";
+import Loader from "../components/Loader";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 class App extends Component {
@@ -11,13 +12,12 @@ class App extends Component {
       type: "",
       url: "https://www.robohash.org/1",
       name: "",
+      loading: false,
     };
   }
 
   clicked = (event) => {
     let new_url = "https://www.robohash.org/".concat(this.state.name);
-    //console.log(this.state.type);
-    //console.log(this.state.type.localeCompare("monster"));
     if (this.state.type.localeCompare("cat") === 0) {
       new_url = new_url.concat("/set_set4");
     } else if (this.state.type.localeCompare("monster") === 0) {
@@ -27,7 +27,14 @@ class App extends Component {
     } else if (this.state.type.localeCompare("human") === 0) {
       new_url = new_url.concat("/set_set5");
     }
-    this.setState({ url: new_url });
+    URL.revokeObjectURL(this.state.url);
+    this.setState({ url: new_url, loading: true });
+    fetch(new_url)
+      .then((response) => response.blob())
+      .then((blobData) => {
+        let fetchedUrl = URL.createObjectURL(blobData);
+        this.setState({ loading: false, url: fetchedUrl });
+      });
   };
 
   selectionChange = (e) => {
@@ -38,16 +45,32 @@ class App extends Component {
     this.setState({ name: e.target.value });
   };
   render() {
+    const loading = this.state.loading;
+
+    //console.log(this.state.loading);
     return (
       <div className="container tc w-50">
         <div className="row">
           <div className="col-md-12">
             <h1 className="f2 avatar mt4">MakeMyAvatar</h1>
+            <p
+              style={{
+                color: "#A31621",
+                fontFamily: "Fanwood Text, serif",
+                fontSize: "18px",
+                fontStyle: "italic",
+                fontWeight: "bold",
+                paddingTop: "5px",
+              }}
+            >
+              Ever wondered how would your name affect your AVATAR image?
+              Well...check it out yourself! ;)
+            </p>
           </div>
         </div>
         <div className="row">
           <div className="fl w-50 pa2 tc">
-            <ImageDisplay url={this.state.url} />
+            {loading ? <Loader /> : <ImageDisplay url={this.state.url} />}
           </div>
           <div className="fl w-50 pa2 tc">
             <Form
@@ -57,9 +80,18 @@ class App extends Component {
             />
           </div>
         </div>
+        <div className="row">
+          <div className="col-md-12">
+            <span className="tc" style={{ fontFamily: "Monotype Corsiva" }}>
+              Robots lovingly delivered by{" "}
+              <a style={{ color: "yellow" }} href="https://www.robohash.org">
+                Robohash.org
+              </a>
+            </span>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
 export default App;
