9baa30736a66576d59e42d37e145689d554a1949
Gauraang P. Kelkar <gauraangkelkar@gmail.com>
2020-04-09 17:24:52 +0530

    Updates

---
+++ precache-manifest.182ef3c48aad0107285b094ef4711a55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c46ff28448abb2a031bed9a4f694e58",
+    "url": "/makemyavatar/index.html"
+  },
+  {
+    "revision": "fda7be74a0bdebda9d59",
+    "url": "/makemyavatar/static/css/2.601ecfa0.chunk.css"
+  },
+  {
+    "revision": "c235ee35bc0b518bbede",
+    "url": "/makemyavatar/static/css/main.65a8cc1b.chunk.css"
+  },
+  {
+    "revision": "fda7be74a0bdebda9d59",
+    "url": "/makemyavatar/static/js/2.6028263b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/makemyavatar/static/js/2.6028263b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c235ee35bc0b518bbede",
+    "url": "/makemyavatar/static/js/main.7f124b4f.chunk.js"
+  },
+  {
+    "revision": "5ecb565c61e4284c2abc",
+    "url": "/makemyavatar/static/js/runtime-main.51fcf61f.js"
+  }
+]);
